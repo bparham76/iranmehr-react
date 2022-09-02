edfca6aa678ff5daeb1102ebe76be4d7711c6d91
Parham Baghizadeh <parham.baghizadeh@yahoo.com>
2022-09-02 18:20:49 +0430

    Header Shadow And Checkups search show refined

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
 function Header() {
-  return <div className='w-full p-2 m-0 text-center text-white md:text-right' style={{backgroundColor: 'var(--theme-color-1)'}}>
-    <div className="px-12 py-4">
-      <h1 className='font-bold text-xl md:text-3xl'>کلینیک دامپزشکی ایران مهر</h1>
-      <p>سیستم آنلاین مدیریت پرونده بیماران</p>
-    </div>
-  </div>
+	return (
+		<div
+			className='w-full p-2 m-0 text-center text-white md:text-right shadow-md shadow-sky-600'
+			style={{ backgroundColor: 'var(--theme-color-1)' }}
+		>
+			<div className='px-12 py-4'>
+				<h1 className='font-bold text-xl md:text-3xl'>
+					کلینیک دامپزشکی ایران مهر
+				</h1>
+				<p>سیستم آنلاین مدیریت پرونده بیماران</p>
+			</div>
+		</div>
+	)
 }
 
 export default Header
--- src/components/dashboard/ShowVisitInfo.jsx
+++ src/components/dashboard/ShowVisitInfo.jsx
@@ -19,10 +19,59 @@ export const ShowVisitInfo = ({ setShow, patient }) => {
 	return (
 		<div
 			ref={Div}
-			className='alert-box fixed top-0 left-0 w-full bg-white m-0 p-0 flex flex-col items-center justify-center z-[1000]'
+			className='alert-box fixed top-0 left-0 w-full bg-white m-0 p-0 flex flex-col gap-4 items-center justify-center z-[1000] '
 		>
-			<p>{patient.name}</p>
-			<p>{patient.sympthoms}</p>
+			<h1 className='font-bold text-3xl'>مشاهده جزئیات مراجعه</h1>
+			<div className='h-3/5 w-full md:w-2/3 grid grid-cols-1 md:grid-cols-2 overflow-y-scroll'>
+				<div className='p-4 text-right'>
+					<table className='w-full'>
+						<tr>
+							<th>تاریخ</th>
+							<td>{patient.date}</td>
+						</tr>
+						<tr>
+							<th>نام</th>
+							<td>{patient.name}</td>
+						</tr>
+						<tr>
+							<th>صاحب</th>
+							<td>{patient.owner}</td>
+						</tr>
+						<tr>
+							<th>جنسیت</th>
+							<td>{patient.gender}</td>
+						</tr>
+						<tr>
+							<th>سن</th>
+							<td>{patient.age}</td>
+						</tr>
+						<tr>
+							<th>نوع</th>
+							<td>{patient.type}</td>
+						</tr>
+						<tr>
+							<th>نژاد</th>
+							<td>{patient.race}</td>
+						</tr>
+					</table>
+				</div>
+				<div className='p-4 text-right'>
+					<table className='w-full whitespace-normal'>
+						<tr>
+							<th className='py-5'>نشانه ها</th>
+							<td>{patient.sympthoms}</td>
+						</tr>
+						<tr>
+							<th className='py-5'>تشخیص</th>
+							<td>{patient.diagnosis}</td>
+						</tr>
+						<tr>
+							<th className='py-5'>تجویز</th>
+							<td>{patient.treatment}</td>
+						</tr>
+					</table>
+				</div>
+			</div>
 			<input
 				type='button'
 				className='btn btn-wide btn-ghost'
