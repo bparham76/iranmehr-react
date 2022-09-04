05ce434a7f573cda2b5ce4c37b6626bc3edf338b
Parham Baghizadeh <parham.baghizadeh@yahoo.com>
2022-09-04 12:31:47 +0430

    Stable Release

--- src/components/shared/PageSection.jsx
+++ src/components/shared/PageSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function PageSection({ children, border = true }) {
 	// let classes = 'border border-solid border-sky-900 rounded-lg'
-	let classes = 'shadow-sky-300 shadow-xl rounded-lg'
+	let classes = 'shadow-violet-300 shadow-xl rounded-lg'
 	return (
 		<div className={`w-full p-2 my-2 mt-10 ${border && classes}`}>
 			{children}
